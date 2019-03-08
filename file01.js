90878c19bc6111e5eadf7c3b858c12becd2dd990
Rishabh sharma <rish6696.rs@gmail.com>
2019-03-08 19:22:29 +0530

    sheldon greeter

--- file01.js
+++ file01.js
@@ -1,1 +1,9 @@
 console.log("this file 1");
+function greet(name)
+{
+    console.log("knock knock knock"+name);
+    console.log("knock knock knock"+name);
+    console.log("knock knock knock"+name);
+    console.log("knock knock knock"+name);
+}
+greet('rishabh ')
